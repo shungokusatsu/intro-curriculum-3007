5fafe554fa87b86daf5e54dd61d9a6057bfae25d
Takuya Fukano <passintoizayoi@gmail.com>
2018-08-29 12:56:59 +0000

    index.jsの修正

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17の倍数である場合 trueを返す
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num){
+    if(num%17===0){
+        return true;
+    }else{
+        return false;
+    }
 }
+
+module.exports = {
+    isMultipleOfSeventeen: isMultipleOfSeventeen
+};
